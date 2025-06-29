12924d164c594cdddbb8da450a7d9a73829681c1
Liasu Teslim <teslimliasu49@gmail.com>
2025-06-28 22:03:37 -0700

    Implemented art section

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,8 @@ import { ScrollTrigger, SplitText } from 'gsap/all'
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import Cocktails from '@/components/Cocktails';
+import About from '@/components/About';
+import Art from '@/components/Art';
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -15,6 +17,8 @@ const page = () => {
       <Navbar/>
       <Hero/>
       <Cocktails/>
+      <About/>
+      <Art/>
     </main>
   )
 }
