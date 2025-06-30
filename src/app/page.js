f9e2c33b13f17164394ee44c69d410531f85542f
Liasu Teslim <teslimliasu49@gmail.com>
2025-06-29 18:08:17 -0700

    final commit

--- src/app/page.js
+++ src/app/page.js
@@ -8,17 +8,21 @@ import Hero from '@/components/Hero';
 import Cocktails from '@/components/Cocktails';
 import About from '@/components/About';
 import Art from '@/components/Art';
+import Menu from '@/components/Menu';
+import Contact from '@/components/Contact';
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const page = () => {
   return (
-    <main>
+    <main className=''>
       <Navbar/>
       <Hero/>
       <Cocktails/>
       <About/>
       <Art/>
+      <Menu/>
+      <Contact/>
     </main>
   )
 }
